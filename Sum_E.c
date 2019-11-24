257b5f28f726d433c5822e14c12d005d23fe3d8c
flyingca <18792779356@163.com>
2019-11-24 23:52:15 +0800

    非递归求整数每一位的数字相加

---
+++ Sum_E.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//int main() {
+//	int n = 0;
+//	printf("please input a number:\n");
+//	scanf("%d", &n);
+//	int i = 0;
+//	int sum = 0;
+//	for (i = n; i; i /= 10) {
+//		printf("%d\n", i % 10);
+//		sum += i % 10;
+//	}
+//	printf("%d\n", sum);
+//	system("pause");
+//	return 0;
+//}
