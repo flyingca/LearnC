990f48bd60c9b1161f167516a9ea90b22136587b
flyingca <18792779356@163.com>
2019-11-24 23:50:09 +0800

    递归与非递归求字符串反转后的输出

---
+++ Str_Inversion.c
@@ -0,0 +1,18 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//
+//void Str_Inversion( char* string) {
+//	if (*string == '\0') {
+//		return 0;
+//	}
+//	Str_Inversion(string + 1);
+//	printf("%c", *string);
+//}
+//
+//int main() {
+//	char* str = "hello";
+//	Str_Inversion(str);
+//	system("pause");
+//	return 0;
+//}
